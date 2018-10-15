5c943b78a92adf9ce63c314313a3caaa7a095cb3
Aiden Nibali <dismaldenizen@gmail.com>
2018-10-16 09:37:52 +1100

    Fix packaging

--- setup.py
+++ setup.py
@@ -9,4 +9,11 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['numpy', 'h5py', 'Pillow', 'tqdm', 'termcolor'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
 )
